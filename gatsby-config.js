b58d5b9e50aa178cd2a95fe8cc0c15c3100e5e37
Drew Smith <smithdrew555@yahoo.com>
2021-12-05 10:07:37 -0500

    added template

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://drew-j-smith.github.io/meal-planner/",
+    title: "meal-planner",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-typescript",
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
+    'gatsby-plugin-postcss',
+  ],
+  pathPrefix: "/meal-planner",
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1 className="bg-red-500 hover:bg-red-700">Home Page</h1>
+    </main>
+  )
+}
+
+export default IndexPage
