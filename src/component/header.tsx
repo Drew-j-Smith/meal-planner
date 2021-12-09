f8ba4f10a6f73ee26490484ed3c3a5e9944d3285
Drew Smith <smithdrew555@yahoo.com>
2021-12-08 21:11:15 -0500

    simplified header

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -12,13 +12,13 @@ class Header extends React.Component<HeaderProps, HeaderState> {
         super(props)
     }
     render() {
-        return <div className="grid grid-cols-1 place-items-center shadow bg-yellow-50">
-            <div className={"flex flex-row w-full max-w-screen-lg"}>
+        return <div className="flex justify-center shadow bg-yellow-50">
+            <div className={"flex w-full max-w-screen-lg"}>
                 <h1 className="flex-grow md:text-5xl sm:text-2xl sm:p-5 p-3">{this.props.title}</h1>
-                <div className="flex sm:text-xl md:text-4xl"> { 
+                {
                     this.props.links.map((el) => 
-                        <Link key={el.name} className="sm:p-5 p-3 hover:bg-yellow-200 transition-colors ease-linear duration-300" to={el.path}>{el.name}</Link>) 
-                } </div>
+                        <Link key={el.name} className="sm:text-xl md:text-4xl sm:p-5 p-3 hover:bg-yellow-200 transition-colors ease-linear duration-300" to={el.path}>{el.name}</Link>) 
+                }
             </div>
         </div>
     }
