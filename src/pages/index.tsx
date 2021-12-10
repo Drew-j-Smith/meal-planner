1c717d00aa076e9951de5ab4a7dfdd3cab74e212
Drew Smith <smithdrew555@yahoo.com>
2021-12-10 09:39:09 -0500

    prevent user from clicking on body when sidebar is open

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ const IndexPage = () => {
         { name: "link2", path: "/"},
         { name: "link3", path: "/"},
         { name: "link4", path: "/"}]}></Header>
-    <div className="grid grid-rows-1 place-items-center">
+    <div className="index-main grid grid-rows-1 place-items-center overflow-y-hidden">
       <div className="grid md:grid-cols-4 grid-rows-4 place-items-center max-w-5xl xl:shadow">
         <RandomSelector elements={["1", "2", "3", "4", "test", "hello world"]}></RandomSelector>
         <RandomSelector elements={["1", "2", "3", "4", "test", "hello world"]}></RandomSelector>
