5b9a0cf367fe8bc9b92f7643aa81da1d571fa434
Drew Smith <smithdrew555@yahoo.com>
2021-12-18 23:54:14 -0500

    added types for handler function

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,6 @@
-exports.handler = async (event: any) => {
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+export const handler: APIGatewayProxyHandler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
